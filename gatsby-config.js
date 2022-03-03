7f52eed96311f7a1a82ecf6dcda8e6a5e63a2521
Skallywag <raysolomon@pm.me>
2022-03-02 18:27:35 -0700

    third commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,11 @@
-require("dotenv").config({
+require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
 module.exports = {
-    siteMetadata: {
-      title: `new`,
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-      
-    ],
+  siteMetadata: {
+    title: `new`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,21 @@
-import * as React from "react"
-import '../css/index.css';
+import * as React from 'react'
+import '../css/index.css'
+
+console.log(process.env.GATSBY_HTTPS_ENDPOINT)
 
 function getEnv(obj) {
-  let text = "";
+  let text = ''
   Object.entries(obj).forEach(([key, value]) => {
-    text += '<p>' + key + ' = ' + value + "</p>";
+    text += '<p>' + key + ' = ' + value + '</p>'
   })
   return text
 }
 
 // data
 const pageStyles = {
-  color: "#232129",
+  color: '#232129',
   padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  fontFamily: '-apple-system, Roboto, sans-serif, serif',
 }
 
 // markup
@@ -21,7 +23,7 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <title>Home Page</title>
-      <p class="codeStyles">{getEnv(process.env)}</p>
+      <p className="codeStyles">{getEnv(process.env)}</p>
     </main>
   )
 }
